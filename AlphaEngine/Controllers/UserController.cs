a1d7e11bf2dcc5ee66efb9847cbd54273405e830
tanjilulanwar <tanjilulanwar@gmail.com>
2024-02-28 17:12:19 +0600

    alignment

--- AlphaEngine/Controllers/UserController.cs
+++ AlphaEngine/Controllers/UserController.cs
@@ -1,13 +1,15 @@
 using Microsoft.AspNetCore.Mvc;
 
-namespace AlphaEngine.Controllers{
- 
-     [Route("api/[controller]/[action]")]
+namespace AlphaEngine.Controllers
+{
+
+    [Route("api/[controller]/[action]")]
     [ApiController]
-    public class UserController:ControllerBase
+    public class UserController : ControllerBase
     {
         [HttpGet]
-        public string Get(int id){
+        public string Get(int id)
+        {
             return $"this record has {id}";
         }
 
